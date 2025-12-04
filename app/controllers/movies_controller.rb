d9eb8e048271af9af7bc84b46edf48276abaabed
gehnarai <gehna.nandwani@chicagobooth.edu>
2025-12-04 20:57:14 +0000

    made search form long

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,11 @@
 class MoviesController < ApplicationController
+    PROVIDER_IDS = {
+    "netflix" => 8, 
+    "prime"   => 9,
+    "disney"  => 337,
+    "hulu"    => 15 
+  }.freeze
+
   def index
     api_key = ENV["TMDB_API_KEY"]
 
@@ -6,29 +13,42 @@ class MoviesController < ApplicationController
 
     min_rating  = params[:min_rating]
     max_runtime = params[:max_runtime]
+    genre_id    = params[:genre_id]
+    query       = params[:query]
+    provider    = params[:provider]
 
-    if min_rating.present? || max_runtime.present?
-      # Use /discover/movie when any filter is present
+     if query.present?
+      # 🔎 Search by title – TMDB /search/movie
       response = HTTParty.get(
-        "#{base_url}/discover/movie",
+        "#{base_url}/search/movie",
         query: {
           api_key: api_key,
           language: "en-US",
-          sort_by: "popularity.desc",
-          "vote_average.gte": min_rating,
-          "with_runtime.lte": max_runtime
-          # later we’ll add: with_genres, with_watch_providers, etc.
-        }.compact  # drops nil values
-      )
-    else
-      # default: trending
-      response = HTTParty.get(
-        "#{base_url}/discover/movie",
-        query: {
-          api_key: api_key,
-          language: "en-US"
+          query: query,
+          include_adult: false
         }
       )
+    else
+      # 🎛 Filtered discover – TMDB /discover/movie
+      discover_query = {
+        api_key: api_key,
+        language: "en-US",
+        sort_by: "popularity.desc",
+        include_adult: false
+      }
+
+      discover_query[:"vote_average.gte"] = min_rating  if min_rating.present?
+      discover_query[:"with_runtime.lte"] = max_runtime if max_runtime.present?
+      discover_query[:with_genres]       = genre_id     if genre_id.present?
+
+      if provider.present?
+        provider_id = PROVIDER_IDS[provider]
+        if provider_id
+          discover_query[:with_watch_providers] = provider_id
+        end
+      end
+
+      response = HTTParty.get("#{base_url}/discover/movie", query: discover_query)
     end
 
     @movies = response["results"] || []
