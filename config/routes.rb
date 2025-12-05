2ac82807402d5a3a03c571cfc9ddb659b6f61bf7
gehnarai <gehna.nandwani@chicagobooth.edu>
2025-12-05 01:08:53 +0000

    fixed routes page and welcome message

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
   # get("/your_first_screen", { :controller => "pages", :action => "first" })
 
-  get "/", controller: "movies", action: "index"
-  get "/movies", controller: "movies", action: "index"
-  get "/movies/:id", controller: "movies", action: "show"
+  get("/", { :controller => "movies", :action => "index"})
+  get("/movies", { :controller => "movies", :action => "index"})
+  get("/movies/:id", { :controller => "movies", :action => "show"})
 end
